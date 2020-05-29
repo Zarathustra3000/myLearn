208091cb57cfe8df2c390c53ad747c9314afee19
Zarathustra3000 <Zarathustra3000@protonmail.com>
2020-05-29 14:18:17 +0300

    Using "try-with-resource" to close resources correctly

--- 10_SQLAndHibernate/JDBCExperiments/src/main/java/Loader.java
+++ 10_SQLAndHibernate/JDBCExperiments/src/main/java/Loader.java
@@ -1,26 +1,41 @@
+import org.apache.commons.dbutils.DbUtils;
+
 import java.sql.*;
 
 public class Loader {
-    public static void main(String[] args) {
 
-        // String url = myURL;
+    private static final String URL = "jdbc:mysql://localhost:3306/skillbox"+
+            "?verifyServerCertificate=false"+
+            "&useSSL=false"+
+            "&requireSSL=false"+
+            "&useLegacyDatetimeCode=false"+
+            "&amp"+
+            "&serverTimezone=UTC";
+    private static final String USER = "user";
+    private static final String PASSWORD = "password";
 
-        // String user = "user";
-        // String password = "password";
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
 
         try {
-            Connection connection = DriverManager.getConnection(url,user,password);
-
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT subscription_date FROM PurchaseList ORDER BY subscription_date");
+            connection = DriverManager.getConnection(URL,USER,PASSWORD);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT PurchaseList.course_name, " +
+                    "COUNT(*)/(TIMESTAMPDIFF(MONTH, MIN(PurchaseList.subscription_date), MAX(PurchaseList.subscription_date)))\n" +
+                    "FROM PurchaseList\n" +
+                    "GROUP BY PurchaseList.course_name;");
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("subscription_date"));
+                System.out.println(resultSet.getString(1) + " - " + resultSet.getString(2));
             }
-            connection.close();
-            statement.close();
-            resultSet.close();
-        } catch (SQLException e) {
+        } catch (Throwable e) {
             e.printStackTrace();
+        } finally {
+            DbUtils.close(resultSet);
+            DbUtils.close(statement);
+            DbUtils.close(connection);
         }
 
     }
